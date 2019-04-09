41a0b21f310c7acf967b096c7793ae718ebf0457
Lawrence Chew <larrychewy@gmail.com>
2019-04-09 16:53:02 -0700

    edited add_item function

--- shopping_cart.py
+++ shopping_cart.py
@@ -5,7 +5,9 @@ class ShoppingCart:
       self.employee_discount = employee_discount
       self.items = items
     def add_item(self, name, price, quantity=1):
-       pass
+       self.items.append(self.name)
+       self.total += quantity * price
+
     def mean_item_price(self):
        pass
 
