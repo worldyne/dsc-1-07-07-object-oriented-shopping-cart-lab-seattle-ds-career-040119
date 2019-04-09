5c0ad81bf46b591dead428efaef033b61bcb9503
Lawrence Chew <larrychewy@gmail.com>
2019-04-09 16:48:44 -0700

    modified init

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,7 +1,9 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, total=0,employee_discount=None,items = []):
-      pass
+      self.total = total
+      self.employee_discount = employee_discount
+      self.items = items
     def add_item(self, name, price, quantity=1):
        pass
     def mean_item_price(self):
