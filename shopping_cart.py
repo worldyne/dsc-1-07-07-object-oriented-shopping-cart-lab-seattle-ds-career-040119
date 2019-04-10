c4351510edb40a757b37a7b2fbab67786541b38a
Lawrence Chew <larrychewy@gmail.com>
2019-04-09 17:41:02 -0700

    edited shopping cart file

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,17 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, total=0,employee_discount=None,items = [],prices = []):
-      self.total = total
-      self.employee_discount = employee_discount
-      self.items = items
+        self.total = total
+        self.employee_discount = employee_discount
+        self.items = items
     def add_item(self, name, price, quantity=1):
-       self.items.append(name)
-       self.total += quantity * price
-       self.prices = []
-       self.prices.append(price)
-       return self.total
+        self.items.append(name)
+        self.total += quantity * price
+        self.prices = []
+        self.prices.append(price)
+        return self.total
     def mean_item_price(self):
-        return self.total / len(self.items)
+        return (self.total / len(self.items))
 
     def median_item_price(self):
         n = len(self.prices)
