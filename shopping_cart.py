14b155797bc5034e974434ec7a743dc94fc9cf53
Lawrence Chew <larrychewy@gmail.com>
2019-04-09 17:16:01 -0700

    edited mean and median

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,21 +1,22 @@
 class ShoppingCart:
     # write your code here
-    def __init__(self, total=0,employee_discount=None,items = []):
+    def __init__(self, total=0,employee_discount=None,items = [],prices = []):
       self.total = total
       self.employee_discount = employee_discount
       self.items = items
     def add_item(self, name, price, quantity=1):
        self.items.append(name)
        self.total += quantity * price
+       self.prices = prices
        return self.total
     def mean_item_price(self):
-       pass
+        return total / len(items)
 
     def median_item_price(self):
-        pass
+        self.prices.median()
 
     def apply_discount(self):
-       pass
+        pass
 
     def void_last_item(self):
         pass
